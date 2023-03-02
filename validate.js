d69d1e8b0dcd32a9ffa581d68d0dbb7ea608c5df
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-02 11:23:49 -0300

    validação com regex no email min e maxlength nos demais com required

--- validate.js
+++ validate.js
@@ -1,35 +1,5 @@
-// function valida(input) {
-//     const tipoDeInput = input.dataset.tipo
 
-//     if(validadores[tipoDeInput]) {
-//         validadores[tipoDeInput](input)
-//     }
-
-//     if(input.validity.valid) {
-//         input.parentElement.classList.remove('input-container--invalido')
-//         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
-//     } else {
-//         input.parentElement.classList.add('input-container--invalido')
-//         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
-//     }
-// }
-
-// const tiposDeErro = [
-//     'valueMissing',
-//     'typeMismatch',
-// ]
-
-// const mensagensDeErro = {
-//     nome: {
-//         valueMissing: 'O campo de nome não pode estar vazio.'
-//     },
-//     email: {
-//         valueMissing: 'O campo de email não pode estar vazio.',
-//         typeMismatch: 'O email digitado não é válido.'
-//     },
-// }
-
-
+/** original  */
 function validarDados(){
     event.preventDefault()
     var error = document.querySelector('#error')
@@ -37,7 +7,7 @@ function validarDados(){
     
     var expresion = /\w+@\w+\.+[a-z]/
 
-    var mensajesError = []
+    var mensajesError = ["Preencha o campo com E-mail válido!"];
     
     if (document.form.name.value=="" ) {
         mensajesError.push("Campo nome é obrigatorio")  
